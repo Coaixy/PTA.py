163f9e772c58aa1f802132b21abbcc1b132b2e65
Coaixy <Coaixy@outlook.com>
2024-10-08 15:52:56 +0800

    7-16

---
+++ 7-16.py
@@ -0,0 +1,14 @@
+
+a = int(input())
+b = int(input())
+
+
+sum_result = a + b
+sub_result = a - b
+mul_result = a * b
+div_result = a / b
+
+print(f"{a} + {b} = {sum_result}")
+print(f"{a} - {b} = {sub_result}")
+print(f"{a} * {b} = {mul_result}")
+print(f"{a} / {b} = {div_result}")
